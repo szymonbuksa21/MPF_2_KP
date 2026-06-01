59a7b3a3c22603edd3d594c58e9de89c6efb9e5e
szymonbuksa21 <194023@student.ue.wroc.pl>
2026-06-01 10:21:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -91,14 +91,14 @@ void calfp(int frates[][3], int fac_rate)
 int main()
 {
     int frates[5][3] = {
-        { 0, 50, 0 },
+        { 0, 5, 0 },   //zmiana wartości
         { 0, 40, 0 },
         { 0, 35, 0 },
         { 0, 6, 0 },
         { 0, 4, 0 }
     };
 
-    int fac_rate = 3;
+    int fac_rate = 3; //zmiana współczynnika 
 
     calfp(frates, fac_rate);
 
